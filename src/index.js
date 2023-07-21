c778adfeac076974e4c347b5714d25c0e1cbae31
Nikhil Kumar <nikhil.pmsnick@gmail.com>
2023-07-21 11:22:08 +0530

    setup the node cron jobs which will sent the email to the destination at a particular interval automatically

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { PORT } = require('./config/serverConfig');
 const bodyParser = require('body-parser');
-const { sendBasicEmail } = require('./service/email-service');
+// const { sendBasicEmail } = require('./service/email-service');
+const cron = require('node-cron');
+
 const startServer = async () => {
     
     const app = express();
@@ -12,12 +14,16 @@ const startServer = async () => {
 
         console.log(`server is running on port : ${PORT}`);
 
-        sendBasicEmail(
-            'support@admin.com' ,
-            'nickrajrandom@gmail.com',
-            'this is a testing email',
-            'hey how are you i hope you like the support'
-        )
+        // sendBasicEmail(
+        //     'support@admin.com' ,
+        //     'nickrajrandom@gmail.com',
+        //     'this is a testing email',
+        //     'hey how are you i hope you like the support'
+        // )
+
+        cron.schedule('* * * * *', () => {
+        console.log('running a task every minute');
+        });
     })
 
 }
